30721cca961bbe01d69c627a3a1d302645e1873f
William Oanta <william.oanta@Williams-MacBook-Pro.local>
2017-11-13 20:08:14 +0200

    Initial Project Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# Generating Faces with GANs in Tensorflow
+
+## Slack Channel
+
+> First and foremost, join us at __[GANs Workshop Slack Channel](https://join.slack.com/t/codiax-gansworkshop/shared_invite/enQtMjcwOTY4NDczMDQxLTFjZjUyNzVkZWExYjJiZjJjYWI1ZjQ2OGFlYmVlMWY0MjRmMjI3YzYzNDIyMmEyYTc3NzEwZmMxNGM1NjgxZWM)__ so that we can keep in sync. Most probably, this channel will be the starting point for a local community of developers interested in **hands-on AI & ML** stuff.
+
+## Data Set Download instructions
+
+For this workshop we will use the __[MNIST](http://yann.lecun.com/exdb/mnist/)__ and __[Celeba](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html)__ datasets. You can download them from __[this](https://drive.google.com/open?id=1ERLFgfDqVEJwG4R5mHihru5DRIx3cs8I)__ google drive folder. The folder contains the two `img_align_celeba` and `mnist` ziped folders. Please create a `data` folder in the project repository's root directory, copy the two dataset folders there and extract them.
+
+The final paths relative to the project root should be: `./data/img_align_celeba` and `./data/mnist`.
+
+## Installation Instructions
+
+### Anaconda or Miniconda
+
+We will be using the __[Anaconda](https://www.anaconda.com/what-is-anaconda/)__ platform for this workshop. Anaconda is
+a *distribution of packages* built for *data science* that comes with **conda**, a *CLI packages* and *environments manager*.
+
+1. Install **Conda CLI**: we are mainly interested in **conda**, therefore you have two options:
+* Install __[Miniconda](https://conda.io/docs/glossary.html#miniconda-glossary)__ - a mini version of Anaconda that includes only **conda and its dependencies**
+  * Download the Python 3.6 installer for your OS from __[here](https://conda.io/miniconda.html)__ and follow __[these](https://conda.io/docs/user-guide/install/index.html)__ instructions to install it.
+* Install the entire __[Anaconda](https://conda.io/docs/glossary.html#anaconda-glossary)__ ecosystem which contains **conda** plus over *720 open source packages*
+  * Download the Python 3.6 installer for your OS and follow the instructions from __[here](https://www.anaconda.com/download/)__.  
+2. Update all the packages in the default *root* environment: `conda upgrade --all`
+  ###### If you get "conda command not found" add `export PATH="/Users/username/anaconda/bin:$PATH"` to your bash config file.
+
+#### Managing Packages
+
+Most used commands:
+* `conda install package_name`
+* `conda update package_name`
+* `conda remove package_name`
+* `conda search search_term`
+* `conda list` - list all installed packages
+
+#### Managing Environments
+Most used commands:
+* `conda create -n env_name list_of_packages`
+* `conda env export > environment.yaml` - save packages to .yaml file
+* `conda env create -f environment.yaml` - create env from __[yaml](http://www.yaml.org/)__ file
+* `source activate my_env` (Mac/Linux)  OR `activate my_env` (Windows) - activate environment
+* `source deactivate` (Mac/Linux) OR `deactivate` (Windows) - deactivate environment
+* `conda env list` - list all environments
+* `conda env remove -n env_name`
+
+For more info check __[this](https://conda.io/docs/user-guide/tasks/index.html)__ and __[this](https://jakevdp.github.io/blog/2016/08/25/conda-myths-and-misconceptions/)__
+
+## Environment Creation & Dependencies
+
+For this workshop create a new environment using the provided gans_workshop.yaml by running:
+* `conda env create -f gans_workshop_env_OS.yaml`
+
+Activate the new created environment using:
+* `source activate gans_workshop` (Mac/Linux)
+* `activate gans_workshop` (Windows)
+
+## Start Test Notebook
+Then run the following command to start a test __[jupyter notebook](http://nbviewer.jupyter.org/)__:
+* `jupyter notebook test.ipynb`
---
+++ utils.py
@@ -0,0 +1,190 @@
+'''
+Most parts were taken from Udacity and modified:
+https://github.com/Piasy/Udacity-DLND/blob/master/face-generation/helper.py
+'''
+
+# Operating system interfaces
+import os
+# Python Image Library for image operations
+from PIL import Image
+# For math stuff
+import math
+# Numpy for N-dimensional array vectorized and broadcasted operations
+import numpy as np
+
+def check_for_data(data_path):
+    '''
+    Checks if the two datasets were placed
+    in the correct folder
+
+    :param data_path: Data folder
+    '''
+
+    # Names for pretty printing
+    celeba = 'CelebA'
+    mnist = 'MNIST'
+
+    # Images folders
+    celeba_folder = 'img_align_celeba'
+    mnist_folder = 'mnist'
+
+    # Images relative paths
+    celeba_path = os.path.join(data_path, celeba_folder)
+    mnist_path = os.path.join(data_path, mnist_folder)
+
+    def data_check_printer(data_dir, data_name):
+        if os.path.exists(data_dir):
+            print('Found {} Data'.format(data_name))
+        else:
+            print('{} Data Not Found'.format(data_name))
+
+    # Check if MNIST exists
+    data_check_printer(celeba_path, mnist)
+
+    # Check if Celeba exists
+    data_check_printer(celeba_path, celeba)
+
+def crop_face(image, face_area):
+    """Crops face from an image"""
+    start_x = (image.size[0] - face_area) // 2
+    end_x = start_x + face_area
+
+    start_y = (image.size[1] - face_area) // 2
+    end_y = start_y + face_area
+
+    cropped_image = image.crop([start_x, start_y, end_x, end_y])
+
+    return cropped_image
+
+def resize_image(image, width, height):
+    """Resizes image to width and height"""
+    return image.resize([width, height], Image.BILINEAR)
+
+def get_image(image_path, width, height, img_mode, process_img=False):
+    """
+    Reads and processes image from image_path
+    :param image_path: Path of image
+    :param width: Width of image
+    :param height: Height of image
+    :param img_mode: Colour Mode of image
+
+    :return: Image
+    """
+
+    image = Image.open(image_path)
+
+    # MNIST images are by default at 28x28
+    if image.size != (width, height) and process_img:
+        # CelebA average square face area
+        face_area = 108
+        # Extract face area from image
+        cropped_image = crop_face(image, face_area)
+        # Resize image
+        image = resize_image(cropped_image, width, height)
+
+    # Convert image to given colour mode
+    image = image.convert(img_mode)
+
+    # Return numpy array
+    return np.array(image)
+
+
+def get_batch(img_files, width, height, img_mode, process_img=False):
+    """
+    Return a batch of images
+    :param img_files: Path to images
+    :param width: Width of image
+    :param height: Height of image
+    :param img_mode: Colour Mode of image
+
+    :return: Image
+    """
+
+    # Load images in memory
+    images = [get_image(img_file, width, height, img_mode, process_img) for img_file in img_files]
+
+    # Convert to numpy for faster processing
+    img_batch = np.array(images).astype(np.float32)
+
+    # Batch needs to have 4 dimensions: (batch_size, width, height, colour_channels)
+    if len(img_batch.shape) < 4:
+        # Add one dimension if needed
+        img_batch = img_batch.reshape(img_batch.shape + (1,))
+
+    return img_batch
+
+# Thanks to math from udacity #
+def images_to_grid(images, mode):
+    """
+    Save images as a square grid
+    :param images: Images to be used for the grid
+    :param mode: The mode to use for images
+    :return: Image of images in a square grid
+    """
+    # Get maximum size for square grid of images
+    save_size = math.floor(np.sqrt(images.shape[0]))
+
+    # Scale to 0-255
+    images = (((images - images.min()) * 255) / (images.max() - images.min())).astype(np.uint8)
+
+    # Put images in a square arrangement
+    images_in_square = np.reshape(
+            images[:save_size*save_size],
+            (save_size, save_size, images.shape[1], images.shape[2], images.shape[3]))
+    if mode == 'L':
+        images_in_square = np.squeeze(images_in_square, 4)
+
+    # Combine images to grid image
+    new_im = Image.new(mode, (images.shape[1] * save_size, images.shape[2] * save_size))
+    for col_i, col_images in enumerate(images_in_square):
+        for image_i, image in enumerate(col_images):
+            im = Image.fromarray(image, mode)
+            new_im.paste(im, (col_i * images.shape[1], image_i * images.shape[2]))
+
+    return new_im
+
+
+class Dataset(object):
+    """
+    Dataset
+    """
+    def __init__(self, dataset_name, data_files):
+        """
+        Initalize the class
+        :param dataset_name: Database name
+        :param data_files: List of files in the database
+        """
+        DATASET_CELEBA_NAME = 'celeba'
+        DATASET_MNIST_NAME = 'mnist'
+        IMAGE_WIDTH = 28
+        IMAGE_HEIGHT = 28
+
+        if dataset_name == DATASET_CELEBA_NAME:
+            self.image_mode = 'RGB'
+            image_channels = 3
+
+        elif dataset_name == DATASET_MNIST_NAME:
+            self.image_mode = 'L'
+            image_channels = 1
+
+        self.data_files = data_files
+        self.shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, image_channels
+
+    def get_batches(self, batch_size):
+        """
+        Generate batches
+        :param batch_size: Batch Size
+        :return: Batches of data
+        """
+        IMAGE_MAX_VALUE = 255
+
+        current_index = 0
+        while current_index + batch_size <= self.shape[0]:
+            data_batch = get_batch(
+                self.data_files[current_index:current_index + batch_size],
+                *self.shape[1:3],
+                self.image_mode)
+
+            current_index += batch_size
+
+            yield data_batch / IMAGE_MAX_VALUE - 0.5
